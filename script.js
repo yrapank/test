b0ee4480fb936029abb743c61a9ed4c8ce250d8c
Yuri <yra.pank@gmail.com>
2019-10-06 13:06:56 +0300

    ferst lesson

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let leftBorderWidth = 1;
-const pi = 3.14;
-// 
+let money = prompt("Ваш бюджет на месяц ?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
 
 
+    },
+    optionalExpenses: {
+
+    },
+    income: [],
+    savings: false
+};
+console.log(appData);
+
+
+let expEx = prompt("введите об статью расходов");
+
+appData.expenses[expEx] = prompt("во сколько это обойдеться,");
+alert("бюджет на 1 день" +appData.budget/30);
